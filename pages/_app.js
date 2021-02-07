7a243dca1fc2d5395045be438a3ffaa52d27fdd3
Hai Ha Nguyen <haicorp87@gmai.com>
2021-02-07 13:06:26 +0100

    adding global styles, logo using styled-components, adding nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
 
+// css
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+// eslint-disable-next-line react/prop-types
 const MyApp = ({ Component, pageProps }) => (
   <Page>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
     <Component {...pageProps} />
   </Page>
 );
--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,11 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en-US">
+      <Html lang='en-US'>
         <body>
+          <Head>
+            <title>Head</title>
+          </Head>
           <Main />
           <NextScript />
         </body>
