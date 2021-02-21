641149d5ff6b043c6c2fa6b40a0f90218a9cd564
Hai Ha Nguyen <haicorp87@gmai.com>
2021-02-21 17:30:07 +0100

    setting up apollo client, using ApolloProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 
 // css
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 // eslint-disable-next-line react/prop-types
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} />
-  </Page>
-);
+const MyApp = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
 
-export default MyApp;
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
